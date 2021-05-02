b85ec9de9e4754dbfe845b093c14d89a6d004ed2
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-02 18:05:54 +0900

    Create Cats-and-a-Mouse.cpp

---
+++ Cats-and-a-Mouse.cpp
@@ -0,0 +1,17 @@
+string catAndMouse(int x, int y, int z)
+{
+    string result = "Cat A";
+    int x_z = std::abs(x-z);
+    int y_z = std::abs(y-z);
+    
+    if (x_z > y_z)
+    {
+        result = "Cat B";
+    }
+    else if (x_z == y_z)
+    {
+        result = "Mouse C";
+    }
+    
+    return result;
+}
