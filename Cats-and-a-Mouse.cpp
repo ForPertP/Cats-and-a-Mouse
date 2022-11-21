c98ee274ef79aa314c1ed6278c5778d3ab1a84b6
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-11-21 11:39:24 +0900

    Update Cats-and-a-Mouse.cpp

--- Cats-and-a-Mouse.cpp
+++ Cats-and-a-Mouse.cpp
@@ -4,17 +4,17 @@ vector<string> split_string(string);
 
 string catAndMouse(int x, int y, int z)
 {
-    string result = "Cat A";
+    string result = "Mouse C";
     int x_z = std::abs(x-z);
     int y_z = std::abs(y-z);
     
-    if (x_z > y_z)
+    if (x_z < y_z)
+    {
+        result = "Cat A";
+    }
+    else if (x_z > y_z)
     {
         result = "Cat B";
-    }
-    else if (x_z == y_z)
-    {
-        result = "Mouse C";
     }
     
     return result;
